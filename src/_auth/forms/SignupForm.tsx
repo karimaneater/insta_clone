f965f4ab245da3366a5ef7280f5d6f01b2248f70
Rollen Ricafort <rollenricafort011@gmail.com>
2024-08-29 16:56:55 +0800

    deployment-err

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from "react-router-dom";
 
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
 import { useForm } from "react-hook-form";
